185150833b1b2865f95ac706eaa114c265a10d24
Debian <debian@vps-4845fc73.vps.ovh.net>
2024-09-04 11:38:42 +0000

    add solana devnet wallet connection

--- go.mod
+++ go.mod
@@ -1,5 +1,11 @@
 module github.com/ioustamora/solanta
 
-go 1.22.1
+go 1.19
 
-require github.com/go-telegram/bot v1.7.2 // indirect
+require (
+	filippo.io/edwards25519 v1.0.0-rc.1 // indirect
+	github.com/blocto/solana-go-sdk v1.30.0 // indirect
+	github.com/go-telegram/bot v1.7.2 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+)
--- solanta.go
+++ solanta.go
@@ -4,9 +4,14 @@ import (
 	"context"
 	"os"
 	"os/signal"
+	"log"
+	"fmt"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
+
+	"github.com/blocto/solana-go-sdk/client"
+	"github.com/blocto/solana-go-sdk/types"
 )
 
 // Send any text message to the bot after the bot has been started
@@ -19,12 +24,23 @@ func main() {
 		bot.WithDefaultHandler(handler),
 	}
 
-	b, err := bot.New("YOUR_BOT_TOKEN_FROM_BOTFATHER", opts...)
+	b, err := bot.New("", opts...)
 	if err != nil {
 		panic(err)
 	}
 
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/hello", bot.MatchTypeExact, helloHandler)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/wallet", bot.MatchTypeExact, walletHandler)
+	
 	b.Start(ctx)
+}
+
+func helloHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:    update.Message.Chat.ID,
+		Text:      "Hello, *" + bot.EscapeMarkdown(update.Message.From.FirstName) + "*",
+		ParseMode: models.ParseModeMarkdown,
+	})
 }
 
 func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
@@ -32,4 +48,61 @@ func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
 		ChatID: update.Message.Chat.ID,
 		Text:   update.Message.Text,
 	})
+	// Print user information
+	log.Printf("Chat ID: %d", update.Message.Chat.ID)
+	log.Printf("User ID: %d", update.Message.From.ID)
+	log.Printf("First Name: %s", update.Message.From.FirstName)
+	log.Printf("Last Name: %s", update.Message.From.LastName)
+	log.Printf("Username: @%s", update.Message.From.Username)
+	log.Printf("Language Code: %s", update.Message.From.LanguageCode)
+	log.Printf("Is Bot: %t", update.Message.From.IsBot)
+	log.Printf("Is Premium: %t", update.Message.From.IsPremium)
+	log.Printf("AddedToAttachmentMenu: %t", update.Message.From.AddedToAttachmentMenu)
+	log.Printf("CanJoinGroups: %t", update.Message.From.CanJoinGroups)
+	log.Printf("CanReadAllGroupMessages: %t", update.Message.From.CanReadAllGroupMessages)
+	log.Printf("SupportInlineQueries: %t", update.Message.From.SupportInlineQueries)
+	log.Printf("CanConnectToBusiness: %t", update.Message.From.CanConnectToBusiness)
+}
+
+func walletHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+    if update.Message != nil {
+        chatID := update.Message.Chat.ID
+
+        // Create a new Solana wallet
+        wallet := types.NewAccount()
+
+        // Airdrop 1 SOL to the new wallet
+        c := client.NewClient("https://api.devnet.solana.com")
+        _, err := c.RequestAirdrop(ctx, wallet.PublicKey.ToBase58(), 1e9) // 1 SOL = 1e9 lamports
+        if err != nil {
+            b.SendMessage(ctx, &bot.SendMessageParams{
+                ChatID: chatID,
+                Text:   "Failed to airdrop 1 SOL: " + err.Error(),
+            })
+            return
+        }
+
+        // Get wallet balance
+        balance, err := c.GetBalance(ctx, wallet.PublicKey.ToBase58())
+        if err != nil {
+            b.SendMessage(ctx, &bot.SendMessageParams{
+                ChatID: chatID,
+                Text:   "Failed to get wallet balance: " + err.Error(),
+            })
+            return
+        }
+
+        // Send wallet details to the user
+        message := fmt.Sprintf(
+            "Wallet Address(Public Key): %s\nPrivate Key: %x\nBalance: %f SOL",
+            wallet.PublicKey.ToBase58(),
+            wallet.PrivateKey,
+            float64(balance)/1e9,
+        )
+
+        b.SendMessage(ctx, &bot.SendMessageParams{
+            ChatID: chatID,
+            Text:   message,
+        })
+    }
 }

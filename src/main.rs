fb7a78e22d8d2635d683b86cf3e6ee03eb32cbb3
stanislav <stanislav@inspreere.com>
2024-09-16 15:57:49 +0200

    sqllite

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use std::hash::Hash;
 use teloxide::{prelude::*, utils::command::BotCommands};
 use solana_sdk::signer::keypair::Keypair;
 use solana_sdk::signer::Signer;
 use std::sync::Arc;
+use rusqlite::{params, Connection, Result};
 
 #[derive(BotCommands, Clone)]
 #[command(rename_rule = "lowercase", description = "These commands are supported:")]
@@ -17,9 +19,38 @@ enum Command {
 }
 
 async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    let db = Arc::new(Connection::open("data.db").unwrap());
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS keys (,
+            user_id INTEGER PRIMARY KEY,
+            user_addr TEXT NOT NULL,
+            user_key TEXT NOT NULL,
+        )",
+        [],
+    ).unwrap();
     match cmd {
-        Command::Start => bot.send_message(msg.chat.id, "i will come soon ...".to_string())
-            .await?,
+        Command::Start => {
+            let user_id_to_check = msg.from.unwrap().id.0;
+
+            // Check if the user_id exists
+            match db.prepare("SELECT EXISTS(SELECT 1 FROM user_data WHERE user_id = ?)") {
+                Ok(mut stmt) => {
+                    match stmt.query_row(params![user_id_to_check], |row| row.get::<usize, bool>(0)) {
+                        Ok(_user_id) => {
+                            bot.send_message(msg.chat.id, "User ID exists in the table.").await?;
+                        }
+                        Err(_) => { println!("Error getting user id"); },
+                    }
+                }
+                Err(_) => {println!("err")}
+            }
+            // let kp = Keypair::new();
+            // let mnemonic = mnemonic::to_string(kp.to_bytes());
+            // bot.send_message(msg.chat.id, kp.pubkey().to_string()).await?;
+            // bot.send_message(msg.chat.id, kp.to_base58_string()).await?;
+            // bot.send_message(msg.chat.id, mnemonic).await?
+            bot.send_message(msg.chat.id, "User ID not exists in the table.").await?
+        },
         Command::Help => bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?,
         Command::Username(username) => {
             bot.send_message(msg.chat.id, format!("Your username is @{username}.")).await?

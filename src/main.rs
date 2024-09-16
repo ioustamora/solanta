4795d530ad72b755d8203badefa848a138b6c2ee
stanislav <stanislav@inspreere.com>
2024-09-16 13:05:39 +0200

    some basic bot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use teloxide::{prelude::*, utils::command::BotCommands};
+use solana_sdk::signer::keypair::Keypair;
+use solana_sdk::signer::Signer;
+use std::sync::Arc;
+
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "start this game ...")]
+    Start,
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "handle a username.")]
+    Username(String),
+    #[command(description = "handle a username and an age.", parse_with = "split")]
+    UsernameAndAge { username: String, age: u8 },
+}
+
+async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Start => bot.send_message(msg.chat.id, "i will come soon ...".to_string())
+            .await?,
+        Command::Help => bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?,
+        Command::Username(username) => {
+            bot.send_message(msg.chat.id, format!("Your username is @{username}.")).await?
+        }
+        Command::UsernameAndAge { username, age } => {
+            bot.send_message(msg.chat.id, format!("Your username is @{username} and age is {age}."))
+                .await?
+        }
+        _ => {
+            bot.send_message(msg.chat.id, "i dont know your command ...".to_string())
+                .await?
+        }
+    };
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    log::info!("Starting command bot...");
+
+    let bot = Bot::from_env();
+
+    Command::repl(bot, answer).await;
 }
